299594d2a8051d56c703ae6f8d1ce609a99d232f
AguapuraVianet <aguapuraufba@gmail.com>
2024-06-20 17:40:42 -0300

    #

--- PhP/set db pdf arquivo.php
+++ PhP/set db pdf arquivo.php
@@ -1,24 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Salvar Documentos</title>
-    <style>
-        input {
-            padding: 10px;
-            font-size: 20px;
-        }
-    </style>
+    <title>Salvar Arquivo</title>
+
 </head>
 
 <body>
-    <h2>Enviar Arquivo</h2>
+    <h2>Enviar Arquivo</h2>  
     <hr>
     <div class="form">
         <form action="" method="POST" enctype="multipart/form-data">
-            <input type="text" name="nome_usuario" placeholder="Digite Seu Nome" required><br>
+            <input type="text" name="nome_usuario" placeholder="Digite seu Nome" required><br>
             <input type="file" name="pdf_arquivo" accept=".pdf" required><br>
             <input type="submit" value="Gravar PDF">
         </form>
@@ -26,31 +20,35 @@
     <hr>
 
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    include('conexao.php'); // Certifique-se de que este arquivo contém a conexão $conn correta
-    $nome_usuario = $_POST['nome_usuario'];
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+    $nome_cliente = $_POST['nome_usuario'];
     $conteudo_pdf = file_get_contents($_FILES['pdf_arquivo']['tmp_name']);
 
-    // Inserir no banco de dados
-       
-    $stmt = $conn->prepare("INSERT INTO documento (nome_usuario, pdf) VALUES (?, ?)");
-    $stmt->bind_param("ss", $nome_usuario, $conteudo_pdf);
 
-    if ($stmt->execute()) {
-        echo "Dados inseridos com sucesso!";
-        } else {
-            echo "Erro ao inserir dados: " . $stmt->error;
-        }
+//inserir no banco de dados
 
-    // Fechar conexão
+$sql = "INSERT INTO `documento`(`nome_cliente`, `pdf_arquivo`) VALUES (?, ?)";
+$stmt = $conn -> prepare ($sql);
+$stmt -> bind_param("ss", $nome_cliente, $conteudo_pdf);
 
-$stmt->close();
+if($stmt -> execute())
+   
+    {
+      echo "Record inserted successfully.";
+      // block of code to process further
+   
+    }else
+   {
+    echo "Record Error.".$stmt -> error;
+    // block of code to process further
+ }
+
+$stmt->close(); 
 $conn->close();
 
 }
 
-?>
+?> 
 
 </body>
-
 </html>
